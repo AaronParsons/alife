a221a43611df601d59ea610a35328a7c6a978ddc
Aaron Parsons <aparsons@berkeley.edu>
2014-03-12 14:07:53 -0700

    Added in DnaRx/Tx subclasses to comm.py

--- src/comm.py
+++ src/comm.py
@@ -1,18 +1,27 @@
-import socket, cPickle, StringIO, hashlib, threading
+import socket, select
+import cPickle, StringIO, hashlib, threading
 import dna, unparse
 
 PORT = 8088
+MAXBUF = 4096
 
-class VolvTX:
+class UdpTx:
     def __init__(self, ip, port=PORT):
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self._dest = (ip, port)
-    def write(self, data):
+    def send(self, data):
         self._sock.sendto(data, self._dest)
 
-class VolvRX:
-    def __init__(self, host, port=PORT, maxbuf=4096):
+class DnaTx(UdpTx):
+    def send_dna(self, dna):
+        data = cPickle.dumps(dna)
+        assert(len(data) <= MAXBUF)
+        self.send(data)
+
+class UdpRx:
+    def __init__(self, host, port=PORT, maxbuf=MAXBUF):
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._sock.setblocking(0)
         self._host = (host,port)
         self.maxbuf = maxbuf
         self._run = False
@@ -20,21 +29,29 @@ class VolvRX:
         self._sock.bind(self._host)
         self._run = True
         while self._run:
-            data, addr = self._sock.recvfrom(self.maxbuf)
-            self.packet_handler(data, addr)
+            ready = select.select([self._sock], [], [], .1)
+            if ready[0]:
+                data, addr = self._sock.recvfrom(self.maxbuf)
+                self.packet_handler(data, addr)
     def packet_handler(self, data, addr):
-        print addr
-        dna = cPickle.loads(data)
-        ast = dna.dna2ast(dna)
-        f = StringIO.StringIO()
-        unparse.Unparser(ast,f)
-        txt = f.getvalue(); f.close()
-        hsh = hashlib.sha224(txt).hexdigest()
-        print hsh
-        print '-'*10
+        print addr, len(data)
     def start(self):
         self._rx_thread = threading.Thread(target=self._listen)
         self._rx_thread.start()
     def quit(self):
         self._run = False
         self._rx_thread.join()
+
+class DnaRx(UdpRx):
+    data_buf = []
+    def packet_handler(self, data, addr):
+        self.data_buf.insert(0, data)
+    def iter_dna(self):
+        while self._run or len(self.data_buf) > 0:
+            if len(self.data_buf) == 0:
+                time.sleep(.01)
+                continue
+            # XXX need to error check this eventually
+            yield cPickle.loads(self.data_buf.pop())
+        return
+
--- tests/comm_test.py
+++ tests/comm_test.py
@@ -3,15 +3,24 @@ from aivolv import comm
 
 class TestComm(unittest.TestCase):
     def test_local(self):
-        rx = comm.VolvRX('localhost')
+        rx = comm.UdpRx('localhost')
         buf = []
         def packet_handler(data, addr): buf.append(data)
         rx.packet_handler = packet_handler
         rx.start()
-        tx = comm.VolvTX('localhost')
-        tx.write('test')
+        tx = comm.UdpTx('localhost')
+        tx.send('test')
         rx.quit()
         self.assertEqual(buf[0], 'test')
+    def test_dna(self):
+        dna = {1:2,3:4}
+        rx = comm.DnaRx('localhost')
+        rx.start()
+        tx = comm.DnaTx('localhost')
+        tx.send_dna(dna)
+        rx.quit()
+        for d in rx.iter_dna():
+            self.assertEqual(dna, d)
 
 if __name__ == '__main__':
     unittest.main()
