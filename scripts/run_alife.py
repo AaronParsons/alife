ca9686b6d36675f676cea8eb20c0464532b9ae2d
Aaron Parsons <aaronrparsons@gmail.com>
2020-05-05 05:37:22 +0000

    Fixed restarting life from a log file.

--- scripts/run_alife.py
+++ scripts/run_alife.py
@@ -6,23 +6,26 @@ import multiprocessing as mp
 import multiprocessing.queues as mpq
 
 def tail(f, lines=100, bufsize=1024):
+    total_lines_wanted = lines
+
     f.seek(0, 2)
-    bytes = f.tell()
-    size = lines
-    block = -1
-    data = []
-    while size > 0 and bytes > 0:
-        if (bytes - bufsize > 0):
-            f.seek(block*bufsize, 2)
-            data.append(f.read(bufsize))
+    block_end_byte = f.tell()
+    lines_to_go = total_lines_wanted
+    block_number = -1
+    blocks = []
+    while lines_to_go > 0 and block_end_byte > 0:
+        if (block_end_byte - bufsize > 0):
+            f.seek(block_number*bufsize, 2)
+            blocks.append(f.read(bufsize))
         else:
             f.seek(0,0)
-            data.append(f.read(bytes))
-        linesFound = data[-1].count('\n')
-        size -= linesFound
-        bytes -= bufsize
-        block -= 1
-    return ''.join(data).splitlines()[-lines:]
+            blocks.append(f.read(block_end_byte))
+        lines_found = blocks[-1].count(b'\n')
+        lines_to_go -= lines_found
+        block_end_byte -= bufsize
+        block_number -= 1
+    all_read_text = b''.join(reversed(blocks))
+    return all_read_text.splitlines()[-total_lines_wanted:]
 
 NUM_BIOS = 10
 
@@ -35,14 +38,15 @@ def logit(s, verbose=True):
 
 rx = ai.comm.DnaRx('localhost')
 rx.start()
-if True: # start alife with the original 'eve' critter
+if False: # start alife with the original 'eve' critter
     ai.eve.run(ai.eve.__file__)
 else: # restart where alife terminated with the last N entries in the log
-    log_tmp = open('log.txt','r')
+    log_tmp = open('log.txt','rb')
     seed_critters = [L.split() for L in tail(log_tmp)]
-    seed_critters = [L[2] for L in seed_critters if len(L) > 3]
+    seed_critters = [L[2].decode('utf-8') for L in seed_critters 
+                     if len(L) > 3]
     log_tmp.close()
-    for i in xrange(10):
+    for i in range(10):
         crit = random.choice(seed_critters)
         logit('# Seeding %s' % crit)
         ai.eve.run('eden/%s.py' % crit)
